90847e1e0ef4b0de68b5cae7487cf630f70bd7fb
Akbarkenzo <113617794+Akbarkenzo@users.noreply.github.com>
2022-09-15 22:05:40 +0700

    Add files via upload

---
+++ volume balok dan kubus akbar.py
@@ -0,0 +1,32 @@
+# menghitung volume balok versi statis
+print("- versi statis")
+print("jika p = 15")
+print("jika l = 8")
+print("jika t = 5")
+p= 15
+l = 8
+t = 5
+v = p*l*t
+print("maka volume baloknya =",v)
+
+# menghitung volume balok versi dinamis
+print("- versi dinamis")
+p = int(input("masukkan nilai p ="))
+l = int(input("masukkan nilai l ="))
+t = int(input("masukkan nilai t ="))
+v = p*l*t
+print("maka volume baloknya =", v)
+
+
+# menghitung volume kubus versi statis 
+print("- versi statis")
+print("jika s = 10")
+s = 10
+v = s*s*s
+print("maka volume kubusnya =",v)
+
+# menghitung volume kubus versi dinamis
+print("- versi dinamis")
+s = int(input("masukkan nilai s ="))
+v = s*s*s
+print("maka volume kubusnya =",v)
